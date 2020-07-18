d2c254b8f30bd3e961adb6c186de3bd4afca5766
Mauro Cortes <mauo.cortes2@gmail.com>
2020-07-18 00:02:23 -0500

    Primeros cambios

---
+++ Conversor_de_monedas.py
@@ -0,0 +1,9 @@
+## Programa convesor de pesos colombianos a dólares
+#Equivalencia del dólar en pesos colombianos
+dolar = 3653
+pesos = input("¿Cuantos pesos colombianos tienes?:")
+pesos = float(pesos)
+
+
+dolares = pesos / dolar
+dolares = print("Tienes $"+ str(dolares) + " dólares")

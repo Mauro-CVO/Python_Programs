c751e7a440062db2a1900a53a764671ae4036a3c
Mauro Cortes <mauo.cortes2@gmail.com>
2020-07-19 15:53:10 -0500

    programa para aproximarse al resultado de un raíz

---
+++ Python_POO/aproximacion.py
@@ -0,0 +1,18 @@
+def run():
+    num = int(input("Escoge un número entero: "))
+    epsilon = 0.01
+    paso = epsilon ** 2
+    ans = 0.0
+
+    while abs(ans ** 2 - num) >= epsilon and ans <= num:
+        print(abs(ans ** 2 - num), ans)
+        ans += paso
+
+    if abs(ans ** 2 - num) >= epsilon:
+        print(f"No se encontro la raíz cuadrada de {num}")
+    else:
+        print(f"La raíz cuadrada de {num} es {ans}")
+
+
+if __name__ == "__main__":
+    run()

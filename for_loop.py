9263905bffdaf36f1251f0d3d8adae670fd62817
Mauro Cortes <mauo.cortes2@gmail.com>
2020-07-18 20:28:37 -0500

    Ciclo for

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+# ##Ciclo While
+# contador = int(input("¿Hasta que numero quieres?: ")) + 1
+# cuenta = 0 
+
+
+# while cuenta != contador:
+#     print(str(cuenta))
+#     cuenta += 1 # == contador = contador + 1
+
+# Ciclo for
+
+# for i in range(1, 1001):
+#     print(i)
+
+for i in range(10):
+    print(11 * i)

330388b26c4cb903cea743c61fa16d7ab7da2276
Mauro Cortes <mauo.cortes2@gmail.com>
2020-07-28 15:59:44 -0500

    hacker rank, between two sets

--- hackerrank/Between_two_sets.py
+++ hackerrank/Between_two_sets.py
@@ -1,7 +1,41 @@
+import math
+import os
+import random
+import re
+import sys
 
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
 from math import gcd
-a = [2, 3, 5]   #will work for an int array of any length
-lcm = a[0]
-for i in a[1:]:
-  lcm = lcm*  i/ gcd(lcm, i)
-print(lcm)
+
+def getTotalX(a, b):
+    count = 0
+    lcm = a[0]
+    for i in a[1:]:
+        lcm = lcm * i/ gcd(lcm, i)
+    for j in b:
+        if j % lcm == 0:
+            count += 1
+        else:
+            continue
+    print(count)
+    
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)

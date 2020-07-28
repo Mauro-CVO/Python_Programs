23162aac9d0d9d820f1c623f9c4dfd03e40fc6af
Mauro Cortes <mauo.cortes2@gmail.com>
2020-07-28 11:44:37 -0500

    Agregue un pequeña descripción

--- hackerrank/kangoroo.py
+++ hackerrank/kangoroo.py
@@ -13,7 +13,8 @@ def kangaroo(x1, v1, x2, v2):
             ans = "YES"
         else:
             ans = "NO"
-        print(ans)
+        
+    print(ans)
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')

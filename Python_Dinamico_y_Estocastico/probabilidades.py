8afac5feabaa4500813bddd3b45622bb5a1da8a1
Mauro Cortes <mauo.cortes2@gmail.com>
2020-07-29 13:13:16 -0500

    probabilidades con eleccion de número de dados y número a estudiar

--- Python_Dinamico_y_Estocastico/probabilidades.py
+++ Python_Dinamico_y_Estocastico/probabilidades.py
@@ -1,32 +1,54 @@
 import random
 
-def tirar_dado(numero_de_tiros):
+def tirar_dado(numero_de_tiros, numero_de_dados):
     secuencia_de_tiros = []
 
-    for i in range(numero_de_tiros):
-        tiro = random.choice([1, 2, 3, 4, 5, 6])
-        secuencia_de_tiros.append(tiro)
-    
-    return secuencia_de_tiros
+    if numero_de_dados == 1:
+        for i in range(numero_de_tiros):    #lanzamientos del primer dado
+            tiro = random.choice([1, 2, 3, 4, 5, 6])
+            secuencia_de_tiros.append(tiro)
+        return secuencia_de_tiros
 
-def run(numero_de_tiros, numero_de_intentos):
+    elif numero_de_dados == 2:
+        for i in range(numero_de_tiros):    #lanzamientos del primer dado
+            tiro = random.choice([1, 2, 3, 4, 5, 6])
+            tiro2 = random.choice([1, 2, 3, 4, 5, 6])
+            x = tiro + tiro2
+            secuencia_de_tiros.append(x)
+        return secuencia_de_tiros
+    
+    else:
+        print("Elige una opción correcta...")
+        main()
+
+
+    
+
+def run(numero_de_tiros, numero_de_intentos, numero_proba, numero_de_dados):
     tiros = []
     for _ in range(numero_de_intentos):
-        secuencia_de_tiros = tirar_dado(numero_de_tiros)
+        secuencia_de_tiros = tirar_dado(numero_de_tiros, numero_de_dados)
         tiros.append(secuencia_de_tiros)
     
-    tiros_con_1 = 0
+    tiros_con_proba = 0 #Cuenta para el número de veces que sale 12
 
     for tiro in tiros:
-        if 1 in tiro:
-            tiros_con_1 += 1
+        if numero_proba in tiro:
+            tiros_con_proba += 1
     
-    probabilidad_de_tiros_con_1 = tiros_con_1 / numero_de_intentos
-    print(f"La probabilidad de obtener por lo menos un 1 en {numero_de_tiros} tiros = {probabilidad_de_tiros_con_1}")
+    probabilidad_de_tiros_con_proba = tiros_con_proba / numero_de_intentos
+    print(f"La probabilidad de obtener por lo menos un {numero_proba} en {numero_de_tiros} tiros = {probabilidad_de_tiros_con_proba}")
+
+def main():
+    numero_de_tiros = int(input("Número de tiros: "))
+    numero_de_intentos = int(input("Cuantas veces correra la simulación: "))
+    numero_proba = int(input("¿ De que número quieres saber la probabilidad? :"))
+    numero_de_dados = int(input("¿Número de dados? (1 o 2): "))
+
+    run(numero_de_tiros, numero_de_intentos, numero_proba, numero_de_dados)
 
 
 if __name__ == "__main__":
-    numero_de_tiros = int(input("Número de tiros: "))
-    numero_de_intentos = int(input("Cuantas veces correra la simulación: "))
+    main()
 
-    run(numero_de_tiros, numero_de_intentos)
+    
